f650c71dcceecccde5faf57516b534d7d268c7fa
Rahul Chaudhary <bossrc64@outlook.com>
2025-12-03 13:50:45 +0530

    Add error handling for folder creation in home page

--- lib/features/notes/presentation/pages/home_page.dart
+++ lib/features/notes/presentation/pages/home_page.dart
@@ -6,6 +6,7 @@ import 'package:notes_bucket/core/constants/app_routes.dart';
 import 'package:notes_bucket/core/theme/app_spacing.dart';
 import 'package:notes_bucket/core/theme/app_text_style.dart';
 import 'package:notes_bucket/core/widgets/app_alert_dialog.dart';
+import 'package:notes_bucket/core/widgets/app_snackbar.dart';
 import 'package:notes_bucket/core/widgets/app_text_field.dart';
 import 'package:notes_bucket/core/widgets/folder_button.dart';
 import 'package:notes_bucket/features/notes/presentation/providers/folder_provider.dart';
@@ -80,7 +81,11 @@ class HomePage extends ConsumerWidget {
                                     );
                                     ref
                                         .read(folderControllerProvider.notifier)
-                                        .createFolder(newFolder);
+                                        .createFolder(newFolder)
+                                        .catchError((e) {
+                                      AppSnackBar.showError(context,
+                                          'Failed to create folder: $e');
+                                    });
                                   }
                                   Navigator.pop(context);
                                 },
