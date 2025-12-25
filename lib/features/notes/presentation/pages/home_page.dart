7459392197052d5f792b66cefb50d40b7079eb73
Rahul Chaudhary <bossrc64@outlook.com>
2025-12-25 13:27:17 +0530

    refactor: Update import for folder dialogs
    This commit replaces the `create_folder_dialog.dart` import with `folder_dialogs.dart` in the `view_all_page.dart` file to consolidate dialog-related imports.

--- lib/features/notes/presentation/pages/home_page.dart
+++ lib/features/notes/presentation/pages/home_page.dart
@@ -12,7 +12,7 @@ import 'package:notes_bucket/core/widgets/notes_app_bar.dart';
 import 'package:notes_bucket/core/widgets/skeletons/folder_grid_view_skeleton.dart';
 import 'package:notes_bucket/features/notes/presentation/pages/view_all_page.dart';
 import 'package:notes_bucket/features/notes/presentation/providers/folder_provider.dart';
-import 'package:notes_bucket/features/notes/presentation/widgets/create_folder_dialog.dart';
+import 'package:notes_bucket/features/notes/presentation/widgets/folder_dialogs.dart';
 import 'package:notes_bucket/features/notes/presentation/widgets/home_page_header.dart';
 import 'package:notes_bucket/features/notes/presentation/widgets/recent_notes.dart';
 
