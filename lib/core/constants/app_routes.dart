aff4f048b1af984e458f6129037e7a2daf77824c
Rahul Chaudhary <bossrc64@outlook.com>
2025-12-27 19:31:05 +0530

    refactor: Rename authentication route constants
    This commit renames the route constants for user sign-in and registration to be more descriptive.
    
    - The `signIn` route is now `userSignIn`.
    - The `signUp` route is now `registerUser`.

--- lib/core/constants/app_routes.dart
+++ lib/core/constants/app_routes.dart
@@ -3,8 +3,8 @@ abstract final class AppRoutes {
   static const String editNotes = '/edit-notes';
   static const String viewAll = '/view-all';
   static const String settings = '/settings';
-  static const String signIn = '/signIn';
-  static const String signUp = '/signUp';
+  static const String registerUser = '/registerUser';
+  static const String userSignIn = '/userSignIn';
   static const String welcome = '/welcome';
 
   static const String about = '/about';
