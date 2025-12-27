62d26786bed7903e5f0b9cf68ca6972f07d9dcd2
Rahul Chaudhary <bossrc64@outlook.com>
2025-12-27 19:29:59 +0530

    feat: Add Google icon asset
    This commit introduces the Google icon SVG to the project's assets.
    
    - A new file, `google_icon.svg`, has been added to the `assets/svgs/` directory.
    - A corresponding constant, `googleIcon`, has been defined in `AppSvgs` to provide a typed accessor for this new asset.

--- lib/core/constants/app_assets.dart
+++ lib/core/constants/app_assets.dart
@@ -1,6 +1,7 @@
 abstract final class AppSvgs {
   static const String moon = 'assets/svgs/moon.svg';
   static const String blueMacFolder = 'assets/svgs/blue_mac_folder.svg';
+  static const String googleIcon = 'assets/svgs/google_icon.svg';
 }
 
 abstract final class AppImages {
