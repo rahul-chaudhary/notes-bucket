cc4a3ef13a3d3f64b4229a9ae29e865adcb3d09d
Rahul Chaudhary <bossrc64@outlook.com>
2026-01-03 07:53:23 +0530

    refactor: Standardize exception handling in auth provider
    This commit updates the error handling within the `doesEmailExistProvider`.
    
    Previously, a `Failure` object was wrapped in a new `Exception`. This change simplifies the code by directly throwing the `failure.message` string, making the exception handling more direct.

--- lib/features/auth/presentation/providers/auth_provider.dart
+++ lib/features/auth/presentation/providers/auth_provider.dart
@@ -62,7 +62,7 @@ class AuthController extends _$AuthController {
       final usecase = ref.read(doesEmailExistUseCaseProvider);
       final useCaseResult = await usecase.call(email);
       return useCaseResult.fold(
-            (failure) => throw Exception(failure.message),
+            (failure) => throw failure.message,
             (exists) => exists,
       );
     });
