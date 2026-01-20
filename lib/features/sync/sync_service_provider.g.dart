04c6bc4b292124d339cd6e172a93da54e42c9cef
Rahul Chaudhary <bossrc64@outlook.com>
2026-01-20 15:54:28 +0530

    feat: Integrate `SyncMonitor` with `SyncService` for state updates

--- lib/features/sync/sync_service_provider.g.dart
+++ lib/features/sync/sync_service_provider.g.dart
@@ -46,4 +46,57 @@ final class SyncServiceProvider
   }
 }
 
-String _$syncServiceHash() => r'569e241354b2a75b85e8d46d63404a759f388e58';
+String _$syncServiceHash() => r'0f5956aa745da67ae75ef768963e7fc4749033cf';
+
+@ProviderFor(SyncMonitor)
+const syncMonitorProvider = SyncMonitorProvider._();
+
+final class SyncMonitorProvider
+    extends $NotifierProvider<SyncMonitor, SyncState> {
+  const SyncMonitorProvider._()
+    : super(
+        from: null,
+        argument: null,
+        retry: null,
+        name: r'syncMonitorProvider',
+        isAutoDispose: false,
+        dependencies: null,
+        $allTransitiveDependencies: null,
+      );
+
+  @override
+  String debugGetCreateSourceHash() => _$syncMonitorHash();
+
+  @$internal
+  @override
+  SyncMonitor create() => SyncMonitor();
+
+  /// {@macro riverpod.override_with_value}
+  Override overrideWithValue(SyncState value) {
+    return $ProviderOverride(
+      origin: this,
+      providerOverride: $SyncValueProvider<SyncState>(value),
+    );
+  }
+}
+
+String _$syncMonitorHash() => r'1312ba4907c1e3147a4b1e8e480b777a67df19f6';
+
+abstract class _$SyncMonitor extends $Notifier<SyncState> {
+  SyncState build();
+  @$mustCallSuper
+  @override
+  void runBuild() {
+    final created = build();
+    final ref = this.ref as $Ref<SyncState, SyncState>;
+    final element =
+        ref.element
+            as $ClassProviderElement<
+              AnyNotifier<SyncState, SyncState>,
+              SyncState,
+              Object?,
+              Object?
+            >;
+    element.handleValue(ref, created);
+  }
+}
